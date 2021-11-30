bd9d035d5302d53bfd0767c6560a99b28290b156
Ismails <shaik.urs@gmail.com>
2021-11-30 12:48:57 +0530

    added read me

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "ec2_instance_id" {
     value = aws_instance.ec2instance.id
 }
+
+output "ec2_instance_private_ip" {
+    value = aws_instance.ec2instance.private_ip
+}
+
+output "ec2_instance_ipv6_address" {
+    value = aws_instance.ec2instance.ipv6_addresses
+}
--- variables.tf
+++ variables.tf
@@ -3,7 +3,16 @@ variable "aws_instance_type" {}
 variable "availability_zone" {}
 variable "subnet_id" {}
 variable "security_groups" {}
-variable "user_data" {}
+variable "user_data" {
+    default = ""
+}
 variable "ec2_instance_iam_profile" {}
-variable "associate_public_ip_address" {}
-variable "ebs_optimized" {}
+variable "associate_public_ip_address" {
+    type = bool
+}
+variable "ebs_optimized" {
+    type = bool
+}
+variable "key_name" {
+    type = string
+}
